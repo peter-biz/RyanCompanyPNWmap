5d5012a664bb94969ddc9bf37e70c0788e7437c6
peter-biz <pgbizoukas@gmail.com>
2024-04-15 14:46:04 -0500

    attempting to web host

--- pnw-map/app/page.tsx
+++ pnw-map/app/page.tsx
@@ -1,8 +1,28 @@
 
+/*This is an app that will display a map of the PNW Hammond Campus and will be interactable.
+The map will be able to display the following:
+- Buildings
+- Parking Lots
+- Roads surrounding the campus
+- Points of interest, (bell tower, etc.) 
+- Campus boundaries
 
+Features:
+- Zoom in and out
+- Search for buildings and rooms
+- Click on buildings to display information
+- Dragability
+- Display of current location(roughly)
+
+*/
+
+//Trying to decide if im gonna draw that map or use a png of the map and overlay the features on top of it.
 export default function Home() {
   return (
-    <p>test</p>
+    <>
+      <h1> Testing feature branch and aws amplify build/deploy</h1>
+      <h2>attempt2</h2>
+    </>
   );
 }
     
