4f475486583a86c1419c2046969a8200e4d7c844
peter-biz <pgbizoukas@gmail.com>
2024-04-24 14:58:06 -0500

    testing sulb and nils and tirmmed code

--- pnw-map/app/components/floor_plan.jsx
+++ pnw-map/app/components/floor_plan.jsx
@@ -8,6 +8,7 @@ function search(image, building, floor){
 
     
     console.log(building);
+    building = building.trim().toUpperCase();
 
     if(building == "Classroom Office Building" || building == "clo"){
              if(floor == 1) image.src = "CLO_1.png";
@@ -39,62 +40,13 @@ function search(image, building, floor){
             else if(floor == 3) image.src = "NILS_3.jpg";
             else home();
         }
+    else if(building == "SULB" || building == "Sulb" || building == "sulb" || building == "Student Union and Library Building" || building == "Library"){
+                if(floor == 1) image.src = "SULB_1.png";
+            else if(floor == 2) image.src = "SULB_2.png";
+            else if(floor == 3) image.src = "SULB_3.png";
+            else home();
+        }
     else {
-        home();
-    }
-}
-
-
-const CLO_N = 41.587205;
-const CLO_S = 41.586626;
-const CLO_E = -87.474978;
-const CLO_W = -87.475711;
-
-const POTT_N = 41.586516;
-const POTT_S = 41.586085;
-const POTT_E = -87.474717;
-const POTT_W = -87.475054;
-
-const ANDR_N = 41.588019;
-const ANDR_S = 41.587366;
-const ANDR_E = -87.474892;
-const ANDR_W = -87.475627;
-
-const GYTE_N = 41.585525;
-const GYTE_S = 41.585004;
-const GYTE_E = -87.474130;
-const GYTE_W = -87.475671;
-
-// NILS Building Coordinates
-const NILSP1 = {lat: 41.58361944832225, lng: -87.47458185241473}; // Top Left
-const NILSP2 = {lat: 41.58361042015047, lng: -87.47343252589033}; // Top Right
-const NILSP3 = {lat: 41.58337819513019, lng: -87.47355926026957}; // Bot Right
-
-
-function showMapByCoordinates(lat, lon){
-    const image = document.getElementById("map_image");
-
-    if( (CLO_S <= lat) && (lat <= CLO_N) && 
-        (CLO_W <= lon) && (lon <= CLO_E) ){
-        image.src = "CLO_1.png";
-    }
-    else if( (POTT_S <= lat) && (lat <= POTT_N) && 
-             (POTT_W <= lon) && (lon <= POTT_E) ){
-        image.src = "POTT_1.png";
-    }
-    else if( (ANDR_S <= lat) && (lat <= ANDR_N) && 
-             (ANDR_W <= lon) && (lon <= ANDR_E) ){
-        image.src = "ANDR_1.png";
-    }
-    else if( (GYTE_S <= lat) && (lat <= GYTE_N) && 
-             (GYTE_W <= lon) && (lon <= GYTE_E) ){
-        image.src = "GYTE_0.png";
-    }
-    else if( (NILSP1.lat <= lat) && (lat <= NILSP2.lat) && 
-             (NILSP1.lng <= lon) && (lon <= NILSP3.lng) ){
-        image.src = "NILS_1.jpg";
-    }
-    else{
         home();
     }
 } 
